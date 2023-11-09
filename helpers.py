54a78994eaa4d5bb5ea74fcf63510a7e5654b73d
tugbatumer <45402554+tugbatumer@users.noreply.github.com>
2023-11-09 15:11:19 +0100

    make file path a parameter

--- helpers.py
+++ helpers.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import csv
 
-def txt_to_csv():
+def txt_to_csv(file_path):
     # Script to convert the ratings and reviews txt files to csv files
-    FILE = 'data/RateBeer/ratings-reviews.txt'
+    # Gets the file path as input
 
     # Read the text file and split it into reviews
-    with open(FILE, 'r', encoding='utf-8') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         reviews_text = file.read().strip().split('\n\n')
 
     # Process each review and extract the data
@@ -20,7 +20,7 @@ def txt_to_csv():
 
 
     # Write the data to a CSV file
-    with open(FILE.replace('txt', 'csv'), 'w', newline='', encoding='utf-8') as file:
+    with open(file_path.replace('txt', 'csv'), 'w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=reviews_data[0].keys())
         writer.writeheader()
         writer.writerows(reviews_data)
