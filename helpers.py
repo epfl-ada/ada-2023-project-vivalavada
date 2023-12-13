f6bbad2237556b597b8f119690fb25599502502b
Luturtun <genceryagiz@gmail.com>
2023-12-13 14:56:52 +0100

    Local and global trends for P3 are now set.

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 import datetime
+import numpy as np
 
 def txt_to_csv(file_path):
     # Script to convert the ratings and reviews txt files to csv files
@@ -32,7 +33,7 @@ def convert_unix_timestamp(unix_timestamp):
     return timestamp
 
 
-def location_style_stats(ratings, users, year, loc_style_threshold):
+def location_style_stats(ratings, users, year, loc_style_threshold, rating_flag):
     year_filter = ratings['year'] == year
     ratings = ratings.loc[year_filter]
     ratings = pd.merge(ratings, users[['user_id', 'nbr_ratings', 'location']], on='user_id', how='inner')
@@ -47,23 +48,54 @@ def location_style_stats(ratings, users, year, loc_style_threshold):
         'location_mean']
     ratings_loc['location_std'] = ratings_groupedby_loc['rating'].std().reset_index(name='location_std')[
         'location_std']
+    ratings_loc['location_std'] = ratings_loc['location_std'].fillna(1)
 
     location_style = pd.merge(location_style, ratings_loc, on='location', how='inner')
 
     location_style['popularity_percentage'] = 100 * (location_style['number'] / location_style['total_loc_number'])
     location_style['mean_rating'] = ratings_groupedby_loc_style['rating'].mean().reset_index(name='mean_rating')[
         'mean_rating']
-    location_style = location_style[
-        ['location', 'style', 'number', 'total_loc_number', 'popularity_percentage', 'mean_rating', 'location_mean',
-         'location_std']]
+
+    location_style_groupedby_style = location_style.groupby('style')
+    style_means = location_style_groupedby_style['popularity_percentage'].mean().to_dict()
+    style_stds = location_style_groupedby_style['popularity_percentage'].std().to_dict()
+
+    location_style['popularity_z_score'] = location_style.apply(lambda x:
+                                        (x['popularity_percentage'] - style_means[x['style']]) / style_stds[x['style']], axis = 1)
+
+
     location_style['z_score'] = (location_style['mean_rating'] - location_style['location_mean']) / location_style[
         'location_std']
-    location_style = location_style[location_style['number'] >= loc_style_threshold]
+
+    location_style = location_style[
+        ['location', 'style', 'number', 'total_loc_number', 'popularity_percentage', 'popularity_z_score',
+        'mean_rating', 'location_mean','location_std', 'z_score']]
+
+    if rating_flag == True:
+        loc_list = location_style['location'].unique()
+        filtered_loc_list = location_style[location_style['number'] >= loc_style_threshold][
+            'location'].unique()
+        locs_filtered_out = set(loc_list) - set(filtered_loc_list)
+        location_style_filtered_out = location_style[
+            location_style['location'].isin(locs_filtered_out)]
+        half_thr = location_style_filtered_out[
+            location_style_filtered_out['number'] >= int(loc_style_threshold / 2)]
+        full_thr = location_style[location_style['number'] >= loc_style_threshold]
+        location_style = pd.concat([full_thr, half_thr])
+
+    else:
+        loc_list = location_style['location'].unique()
+        filtered_loc_list = location_style[location_style['popularity_z_score'] > 1.0]['location'].unique()
+        locs_filtered_out = set(loc_list) - set(filtered_loc_list)
+        location_style_filtered_out = location_style[location_style['location'].isin(locs_filtered_out)]
+        half_thr = location_style_filtered_out[location_style_filtered_out['popularity_z_score'] > 0.5]
+        full_thr = location_style[location_style['popularity_z_score'] > 1.0]
+        location_style = pd.concat([full_thr, half_thr])
 
     return location_style
 
 
-def location_brewery_country_stats(ratings, users, breweries, year, loc_brewery_threshold):
+def location_brewery_country_stats(ratings, users, breweries, year, loc_brewery_threshold, rating_flag):
 
     year_filter = ratings['year'] == year
     ratings = ratings.loc[year_filter]
@@ -86,18 +118,44 @@ def location_brewery_country_stats(ratings, users, breweries, year, loc_brewery_
         'location_mean']
     ratings_loc['location_std'] = ratings_gb_loc['rating'].std().reset_index(name='location_std')[
         'location_std']
+    ratings_loc['location_std'] = ratings_loc['location_std'].fillna(1)
 
     location_brewery_country = pd.merge(location_brewery_country, ratings_loc, on='location', how='inner')
 
     location_brewery_country['popularity_percentage'] = 100 * (location_brewery_country['number'] / location_brewery_country['total_loc_number'])
+    location_brewery_country_groupedby_brew_location = location_brewery_country.groupby('brewery_location')
+    brew_loc_means = location_brewery_country_groupedby_brew_location['popularity_percentage'].mean().to_dict()
+    brew_loc_stds = location_brewery_country_groupedby_brew_location['popularity_percentage'].std().to_dict()
+
+    location_brewery_country['popularity_z_score'] = location_brewery_country.apply(lambda x:
+                                                                (x['popularity_percentage'] - brew_loc_means[x['brewery_location']]) /
+                                                                brew_loc_stds[x['brewery_location']], axis=1)
+
+    location_brewery_country['z_score'] = (location_brewery_country['mean_rating'] - location_brewery_country[
+        'location_mean']) / location_brewery_country['location_std']
+
 
     location_brewery_country = location_brewery_country[
-        ['location', 'brewery_location', 'number', 'total_loc_number', 'popularity_percentage', 'mean_rating', 'location_mean',
-         'location_std']]
-    location_brewery_country['z_score'] = (location_brewery_country['mean_rating'] - location_brewery_country['location_mean']) / location_brewery_country[
-        'location_std']
+        ['location', 'brewery_location', 'number', 'total_loc_number', 'popularity_percentage', 'popularity_z_score',
+        'mean_rating', 'location_mean','location_std', 'z_score']]
 
-    location_brewery_country = location_brewery_country[location_brewery_country['number'] >= loc_brewery_threshold]
+    if rating_flag == True:
+        loc_list = location_brewery_country['location'].unique()
+        filtered_loc_list = location_brewery_country[location_brewery_country['number'] >= loc_brewery_threshold]['location'].unique()
+        locs_filtered_out = set(loc_list) - set(filtered_loc_list)
+        location_brewery_country_filtered_out = location_brewery_country[location_brewery_country['location'].isin(locs_filtered_out)]
+        half_thr = location_brewery_country_filtered_out[location_brewery_country_filtered_out['number'] >= int(loc_brewery_threshold/2)]
+        full_thr = location_brewery_country[location_brewery_country['number'] >= loc_brewery_threshold]
+        location_brewery_country = pd.concat([full_thr, half_thr])
+
+    else:
+        loc_list = location_brewery_country['location'].unique()
+        filtered_loc_list = location_brewery_country[location_brewery_country['popularity_z_score'] > 1.0]['location'].unique()
+        locs_filtered_out = set(loc_list) - set(filtered_loc_list)
+        location_brewery_filtered_out = location_brewery_country[location_brewery_country['location'].isin(locs_filtered_out)]
+        half_thr = location_brewery_filtered_out[location_brewery_filtered_out['popularity_z_score'] > 0.5]
+        full_thr = location_brewery_country[location_brewery_country['popularity_z_score'] > 1.0]
+        location_brewery_country = pd.concat([full_thr, half_thr])
 
     return location_brewery_country
 
